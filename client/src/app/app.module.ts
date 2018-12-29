108caf94ca8a7451cff357729ec675c6ef60a2ea
Keenan Andrea <Keenandrea91@gmail.com>
2018-12-29 02:14:31 -0600

    first commit

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 import { AppRoutingModule } from './app-routing.module'; 
 
 import { AppComponent } from './app.component';
@@ -8,6 +9,10 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { RegisterComponent } from './components/register/register.component';
+import { AuthService } from './services/auth.service';
+import { LoginComponent } from './components/login/login.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { FlashMessagesModule } from 'angular2-flash-messages';
 
 @NgModule({
   declarations: [
@@ -16,13 +21,17 @@ import { RegisterComponent } from './components/register/register.component';
     HomeComponent,
     DashboardComponent,
     RegisterComponent,
+    LoginComponent,
+    ProfileComponent,
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     AppRoutingModule,
     ReactiveFormsModule,
+    FlashMessagesModule.forRoot()
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- routes/authentication.js
+++ routes/authentication.js
@@ -1,4 +1,6 @@
 const User = require('../models/user');
+const jwt = require('jsonwebtoken');
+const config = require('../config/database');
 
 module.exports = (router) => {
 
@@ -51,7 +53,103 @@ router.post('/register', (req, res) => {
       }
 });
 
+router.get('/checkEmail/:email', (req, res) => {
+    if(!req.params.email) {
+        res.json({ success: false, message: 'Email was not provided'});
+    } else {
+        User.findOne({ email: req.params.email}, (err, user) => {
+            if(err) {
+                res.json({ success: false, message: err });
+            } else {
+                if(user) {
+                    res.json({ success: false, message: 'Email is already taken'});
+                } else {
+                    res.json({ success: true, message: 'Email is available'});
+                }
+            }
+        });
+    }
+});
 
+router.get('/checkUsername/:username', (req, res) => {
+    if(!req.params.username) {
+        res.json({ success: false, message: 'Username was not provided'});
+    } else {
+        User.findOne({ username: req.params.username}, (err, user) => {
+            if(err) {
+                res.json({ success: false, message: err });
+            } else {
+                if(user) {
+                    res.json({ success: false, message: 'Username is already taken'});
+                } else {
+                    res.json({ success: true, message: 'Username is available'});
+                }
+            }
+        });
+    }
+});
+
+router.post('/login', (req, res) => {
+    if(!req.body.username) {
+        res.json({ success: false, message: 'No username was provided'});
+    } else {
+        if(!req.body.password) {
+            res.json({ success: false, message: 'No password was provided'});
+        } else {
+            User.findOne({ username: req.body.username.toLowerCase() }, (err, user) => {
+                if(err) {
+                    res.json({ success: false, message: err });
+                } else {
+                    if(!user) {
+                        res.json({ success: false, message: 'Username not found'});
+                    } else {
+                        const validPassword = user.comparePassword(req.body.password);
+                        if(!validPassword) {
+                            res.json({ success: false, message: 'Password invalid' });
+                        } else {
+                            const token = jwt.sign({userId: user._id}, config.secret, { expiresIn: '24h' });
+                            res.json({ success: true, message: 'Success', token: token, user: { username: user.username } });
+
+
+
+                            res.json({ success: true, message: 'Sucess' });
+                        }
+                    }
+                }
+            })
+        }
+    }
+});
+
+router.use((req, res, next) => {
+   const token = req.headers['authorization'];
+   if(!token) {
+       res.json({ success: false, message: 'No token provided'});
+   } else {
+       jwt.verify(token, config.secret, (err, decoded) => {
+           if(err) {
+               res.json({ success: false, message: 'Token invalid: ' + err});
+           } else {
+               req.decoded = decoded;
+               next();
+           }
+       });
+   }
+});
+
+router.get('/profile', (req, res) => {
+   User.findOne({ _id: req.decoded.userId }).select('username email').exec((err, user) => {
+       if(err) {
+           res.json({ success: false, messages: err });
+       } else {
+           if(!user) {
+               res.json({ success: false, message: 'User not found' });
+           } else {
+               res.json({ success: true, user: user });
+           }
+       }
+   }); 
+});
 
     return router;
 }
